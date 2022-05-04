a8b138970a20592cb10cf023fa010b7a17e8f5dd
likaikai <593496637@qq.com>
2022-05-04 08:44:47 +0800

    vite

---
+++ webpack-learn/09_vite/src/vue/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="title">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  color: antiquewhite;
+}
+</style>
