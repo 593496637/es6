40c70a42dd2f62670c4f7bcca64fa4f84dbe6927
云清淡雅 <593496637@qq.com>
2023-03-27 17:03:56 +0800

    react

---
+++ react/05_购物车阶段案例/data.js
@@ -0,0 +1,37 @@
+const books = [
+  {
+    id: 1,
+    title: "红楼梦",
+    author: "曹雪芹",
+    year: 1791,
+    price: 20.99
+  },
+  {
+    id: 2,
+    title: "西游记",
+    author: "吴承恩",
+    year: 1592,
+    price: 18.99
+  },
+  {
+    id: 3,
+    title: "水浒传",
+    author: "施耐庵",
+    year: 1368,
+    price: 16.99
+  },
+  {
+    id: 4,
+    title: "三国演义",
+    author: "罗贯中",
+    year: 1368,
+    price: 14.99
+  },
+  {
+    id: 5,
+    title: "金瓶梅",
+    author: "兰陵笑笑生",
+    year: 1610,
+    price: 12.99
+  }
+];
