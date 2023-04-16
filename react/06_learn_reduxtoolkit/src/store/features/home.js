59729a61e9a72ab0d0793fed2db841a94a88e448
likaikai <593496637@qq.com>
2023-04-17 00:27:22 +0800

    home

--- react/06_learn_reduxtoolkit/src/store/features/home.js
+++ react/06_learn_reduxtoolkit/src/store/features/home.js
@@ -1,8 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const fetchHomeData = createAsyncThunk('home/fetchHomeData', async () => {
+export const fetchHomeData = createAsyncThunk('home/fetchHomeData', async (extraInfo, { dispatch, getState }) => {
+
+  // 这里可以写异步请求
   const { data } = await axios.get('http://123.207.32.32:8000/home/multidata');
+  // 获取数据，并且dispatch到reducer中 ：可以不做任何处理，直接返回数据
+  // dispatch(setBannerList(data.data.banner.list));
+  // dispatch(setRecommendList(data.data.recommend.list));
+
+  // 返回数据, 会被传递到fulfilled的回调函数中
   return data;
 })
 
@@ -20,19 +27,51 @@ const homeSlice = createSlice({
       state.recommendList = payload;
     }
   },
-  extraReducers: {
-    [fetchHomeData.pending]: (state, { payload }) => {
+  // 新的写法，使用builder.addCase()
+  extraReducers: builder => {
+
+    // 第三种写法
+    // 还可以链式调用
+    builder.addCase(fetchHomeData.pending, (state, action) => {
       console.log('pending');
-    },
-    [fetchHomeData.fulfilled]: (state, { payload }) => {
+    }).addCase(fetchHomeData.fulfilled, (state, action) => {
       console.log('fulfilled');
-      state.bannerList = payload.data.banner.list;
-      state.recommendList = payload.data.recommend.list;
-    },
-    [fetchHomeData.rejected]: (state, { payload }) => {
+      state.bannerList = action.payload.data.banner.list;
+      state.recommendList = action.payload.data.recommend.list;
+    }).addCase(fetchHomeData.rejected, (state, action) => {
       console.log('rejected');
-    }
-  }
+    });
+
+    // 第二种写法
+    // builder.addCase(fetchHomeData.pending, (state, action) => {
+    //   console.log('pending');
+    // });
+    // builder.addCase(fetchHomeData.fulfilled, (state, action) => {
+    //   console.log('fulfilled');
+    //   state.bannerList = action.payload.data.banner.list;
+    //   state.recommendList = action.payload.data.recommend.list;
+    // });
+    // builder.addCase(fetchHomeData.rejected, (state, action) => {
+    //   console.log('rejected');
+    // });
+  },
+
+  // 第一种写法
+  // 旧的写法，使用extraReducers的属性表示法
+  // extraReducers的属性表示法已经被弃用，使用builder.addCase()替代
+  // extraReducers: {
+  //   [fetchHomeData.pending]: (state, { payload }) => {
+  //     console.log('pending');
+  //   },
+  //   [fetchHomeData.fulfilled]: (state, { payload }) => {
+  //     console.log('fulfilled');
+  //     state.bannerList = payload.data.banner.list;
+  //     state.recommendList = payload.data.recommend.list;
+  //   },
+  //   [fetchHomeData.rejected]: (state, { payload }) => {
+  //     console.log('rejected');
+  //   }
+  // }
 })
 
 export default homeSlice.reducer
