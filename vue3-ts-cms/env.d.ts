bd82943ee565ab3819279d457a5e0b3ec3e90f9e
云清淡雅 <593496637@qq.com>
2023-08-12 18:50:01 +0800

    vue3-ts-cms

---
+++ vue3-ts-cms/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+// 作用：解决ts中引入vue文件报错的问题
+// 问题原因：ts默认不支持导入vue文件，需要声明一下
+// 可以不用声明，但是必须安装volar插件，否则会报错
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
