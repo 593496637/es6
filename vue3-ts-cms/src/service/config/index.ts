06f0b1891d0d01c8985cbab3fa8e679217205c6d
云清淡雅 <593496637@qq.com>
2023-08-15 10:29:31 +0800

    add:env

--- vue3-ts-cms/src/service/config/index.ts
+++ vue3-ts-cms/src/service/config/index.ts
@@ -1,9 +1,10 @@
-let BASE_URL = ''
-if (import.meta.env.DEV) {
-  BASE_URL = 'http://www.codercba.com:8000'
-} else {
-  BASE_URL = 'http://www.codercba.com:8000'
-}
+// if (import.meta.env.DEV) {
+//   BASE_URL = 'http://www.codercba.com:8000'
+// } else {
+//   BASE_URL = 'http://www.codercba.com:8000'
+// }
+
+const BASE_URL = import.meta.env.VITE_URL
 
 export { BASE_URL }
 export const TIME_OUT = 10000
