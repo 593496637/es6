7078d9e8250d331b59e08165de3c2244339f8027
云清淡雅 <593496637@qq.com>
2023-11-07 10:05:54 +0800

    feat:parcel

--- Parcel-learn/parcel/src/app.js
+++ Parcel-learn/parcel/src/app.js
@@ -1,7 +1,19 @@
+// import $ from 'jquery';
+import './styles.css';
 import foo from './foo.js';
+import logo from './abc.png';
 foo.bar();
 
 console.log('hellow world!');
+
+// $(document.body).append('<div>hello world</div>');
+// $(document.body).append(`<img src="${logo}" />`);
+
+// 动态导入jQuery后，会自动拆分成一个单独的包，这个包会在使用时才会加载
+import('jquery').then(($) => {
+  $(document.body).append('<div>hello world</div>');
+  $(document.body).append(`<img src="${logo}" />`);
+});
 
 if (module.hot) {
   module.hot.accept(() => {
