033e9f84648240844a1b3abfd8cd7e1011f74a6c
云清淡雅 <593496637@qq.com>
2023-11-08 13:53:48 +0800

    feat:esbuild+swc+TS

--- typescript/ts-小满教程/topics/27.esbuild+swc构建ts/config.ts
+++ typescript/ts-小满教程/topics/27.esbuild+swc构建ts/config.ts
@@ -1,30 +1,36 @@
-import esbuild from 'esbuild';
-import swc from '@swc/core';
-import fs from 'node:fs';
-
+import esbuild from "esbuild"; //打包工具
+import swc from "@swc/core"; //类似于babel es6 转 es5
+import fs from "node:fs";
 await esbuild.build({
-  entryPoints: ['./index.ts'],
-  treeShaking: true,
-  bundle: true,
+  entryPoints: ["./src/main.ts"], //入口文件
+  bundle: true, //模块单独打包
   loader: {
-    '.js': 'js',
-    '.ts': 'ts',
-    '.jsx': 'jsx',
-    '.tsx': 'tsx',
+    ".js": "js",
+    ".ts": "ts",
+    ".jsx": "jsx",
+    ".tsx": "tsx",
+  },
+  treeShaking: true,
+  define: {
+    "process.env.NODE_ENV": '"production"',
   },
   plugins: [
     {
-      name: 'swc-loader',
+      //实现自定义loader
+      name: "swc-loader",
       setup(build) {
-        build.onLoad({ filter: /\.(js|ts|jsx|tsx)/ }, (args) => {
-          const content = fs.readFileSync(args.path, 'utf-8');
-          console.log(content);
+        build.onLoad({ filter: /\.(js|ts|tsx|jsx)/ }, (args) => {
+          // console.log(args);
+          const content = fs.readFileSync(args.path, "utf-8");
+          const { code } = swc.transformSync(content, {
+            filename: args.path,
+          });
           return {
-            contents: '',
+            contents: code,
           };
         });
       },
     },
   ],
-  outdir: 'dist',
+  outdir: "dist",
 });
--- typescript/ts-小满教程/topics/27.esbuild+swc构建ts/src/main.ts
+++ typescript/ts-小满教程/topics/27.esbuild+swc构建ts/src/main.ts
@@ -5,3 +5,5 @@ export const b: string = 'hello';
 let x = 1;
 let fn = () => 123;
 console.log(x, fn);
+
+export {}
